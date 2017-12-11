3a9411d4dd8906cebaff793a49c5ada92bd7feb0
jimzi94 <jimmyxavierduarte@gmail.com>
2017-12-11 17:58:51 -0600

    Actualizacion

--- Practica1/src/practica1/Practica1.java
+++ Practica1/src/practica1/Practica1.java
@@ -18,11 +18,19 @@ public class Practica1 {
      */
     
     static Scanner leer=new Scanner(System.in);
-  
+    static Integer[][] movimientos=new Integer[100][3];
     static String nombre_jugador="";
     static String[][] mapa=new String[71][37];
+    static int tamaño=0;
+    static int puntuacion;
+    static int posicion_x=35;
+               static int posicion_y=17;
     public static void main(String[] args) {
-      menu();
+       movimientos[0][0]=35;
+         movimientos[0][1]=17;
+         puntuacion=10;
+        menu();
+      
     }
      public static void mapa(){
          mapa[0][0]="#";
@@ -44,7 +52,9 @@ public class Practica1 {
       }
       for(int controladora_filas=1;controladora_filas<35;controladora_filas++){
       for(int controladora_columnas=1;controladora_columnas<69;controladora_columnas++){
+           
           mapa[controladora_columnas][controladora_filas]=" ";
+      
       }}
       for(int controladora=0;controladora<70;controladora++){
          mapa[controladora][35]="#";
@@ -54,25 +64,36 @@ public class Practica1 {
       {
           int pared_x=(int)(Math.random()*69)+1;
           int pared_y=(int)(Math.random()*34)+1;
-          mapa[pared_x][pared_y]="#";
+          if(!mapa[pared_x][pared_y].equals("O"))
+          {
+           mapa[pared_x][pared_y]="#";
+          }
+         
       }
        for(int comida_aleatoria=0;comida_aleatoria<40;comida_aleatoria++)
       {
           int pared_x=(int)(Math.random()*69)+1;
           int pared_y=(int)(Math.random()*34)+1;
+           if(!mapa[pared_x][pared_y].equals("O"))
+          {
           mapa[pared_x][pared_y]="%";
+          }
       }
        for(int daño_aleatoria=0;daño_aleatoria<30;daño_aleatoria++)
       {
           int pared_x=(int)(Math.random()*69)+1;
           int pared_y=(int)(Math.random()*34)+1;
+           if(!mapa[pared_x][pared_y].equals("O"))
+          {
           mapa[pared_x][pared_y]="$";
+          }
       }
        mapa[35][17]="0";
      
        
       
     }
+     
      public static void refresh_map(){
       for(int mapeo_y=0;mapeo_y<36;mapeo_y++)
       {
@@ -82,7 +103,18 @@ public class Practica1 {
       }
       System.out.println();
       }
+      System.out.println();
+      System.out.println("Puntuacion: "+puntuacion );
      }
+   public static void rastreo(int new_x,int new_y){
+       
+       
+            movimientos[1][0]=new_x;
+       movimientos[1][1]=new_y;
+       mapa[movimientos[0][0]][movimientos[0][1]]=" ";
+       movimientos[0][0]=movimientos[1][0];
+       movimientos[0][1]=movimientos[1][1];
+   }
     public static void menu(){
           String selector_menu = "";
         System.out.println("1)Iniciar Juego");
@@ -92,19 +124,365 @@ public class Practica1 {
         System.out.println("Ingrese una Opcion:");
          
         selector_menu=leer.next(); 
+      
         switch(selector_menu){
             case "1": 
-                System.out.print("Ingrese su nombre: ");
+                String movimiento = "0";
+             
+                  System.out.print("Ingrese su nombre: ");
               
                 nombre_jugador=leer.next();
-                
-               mapa();
-                 
+                 mapa();
+                 refresh_map();
+                 boolean ciclo=true;
+                 while(ciclo){
+                 System.out.println("Escriba su comando porfavor");
+                 movimiento=leer.next();
+                switch(movimiento){
+                    
+                    case "a":
+                        if(mapa[posicion_x-1][posicion_y].equals(" "))
+                        {
+                           
+                            mapa[posicion_x-1][posicion_y]="0";
+                          
+                            rastreo((posicion_x-1)+tamaño,posicion_y);
+                           refresh_map();
+                            posicion_x--;
+                            
+                            
+                        }
+                        else if(mapa[posicion_x-1][posicion_y].equals("%")){
+                     mapa[posicion_x-1][posicion_y]="0";
+                            
+                            mapa[posicion_x][posicion_y]="0";
+                            rastreo(posicion_x+1,posicion_y);
+                            refresh_map();
+                            posicion_x--;
+                            puntuacion=puntuacion+10;
+                            tamaño=tamaño+1;
+                            refresh_map();
+                        }             
+                        else if(mapa[posicion_x-1][posicion_y].equals("$")){
+                    
+                            
+                            
+                           
+                            mapa[posicion_x-1][posicion_y]="0";
+                          
+                            rastreo((posicion_x+1)-tamaño,posicion_y);
+                           refresh_map();
+                            posicion_x--;
+                            puntuacion=puntuacion-10;
+                            tamaño--;
+                            refresh_map();
+                        }        
+                        break;
+                    case "d":
+                      if(mapa[posicion_x+1][posicion_y].equals(" "))
+                        {
+                           
+                            mapa[posicion_x+1][posicion_y]="0";
+                          
+                            rastreo((posicion_x+1)-tamaño,posicion_y);
+                          
+                            posicion_x++;
+                             refresh_map();
+                            
+                        }
+                         else if(mapa[posicion_x+1][posicion_y].equals("%")){
+                     mapa[posicion_x+1][posicion_y]="0";
+                            
+                            mapa[posicion_x][posicion_y]="0";
+                            rastreo(posicion_x-1,posicion_y);
+                       
+                            posicion_x++;
+                            puntuacion=puntuacion+10;
+                            tamaño=tamaño+1;
+                            refresh_map();
+                        }             
+                        
+                        
+                      else if(mapa[posicion_x+1][posicion_y].equals("$")){
+                    
+                            
+                            
+                           
+                            mapa[posicion_x+1][posicion_y]="0";
+                          
+                            rastreo((posicion_x+1)-tamaño,posicion_y);
+                           refresh_map();
+                            posicion_x++;
+                            puntuacion=puntuacion-10;
+                            tamaño--;
+                            refresh_map();
+                        }        
+                        break;
+                        
+                        case "s":
+                        if(mapa[posicion_x][posicion_y+1].equals(" "))
+                        {
+                           
+                            mapa[posicion_x][posicion_y+1]="0";
+                          
+                            rastreo((posicion_x),(posicion_y+1)-tamaño);
+                           refresh_map();
+                            posicion_y++;
+                            
+                            
+                        }
+                        else if(mapa[posicion_x][posicion_y+1].equals("%")){
+                     mapa[posicion_x][posicion_y+1]="0";
+                            
+                            mapa[posicion_x][posicion_y]="0";
+                            rastreo(posicion_x,posicion_y-1);
+                          
+                            posicion_y++;
+                            puntuacion=puntuacion+10;
+                            tamaño=tamaño+1;
+                            refresh_map();
+                        }             
+                        else if(mapa[posicion_x][posicion_y+1].equals("$")){
+                    
+                            
+                            
+                           
+                            mapa[posicion_x][posicion_y+1]="0";
+                          
+                            rastreo((posicion_x)-tamaño,posicion_y-1);
+                           refresh_map();
+                            posicion_y++;
+                            puntuacion=puntuacion-10;
+                            tamaño--;
+                            refresh_map();
+                        }        
+                        
+                       
+                        break;
+                         case "w":
+                        if(mapa[posicion_x][posicion_y-1].equals(" "))
+                        {
+                           
+                            mapa[posicion_x][posicion_y-1]="0";
+                          
+                            rastreo((posicion_x),posicion_y-1+tamaño);
+                           refresh_map();
+                            posicion_y--;
+                            
+                            
+                        }
+                        else if(mapa[posicion_x][posicion_y-1].equals("%")){
+                     mapa[posicion_x][posicion_y-1]="0";
+                            
+                            mapa[posicion_x][posicion_y]="0";
+                            rastreo(posicion_x,posicion_y+1);
+                          
+                            posicion_y--;
+                            puntuacion=puntuacion+10;
+                            tamaño=tamaño+1;
+                            refresh_map();
+                        }             
+                        else if(mapa[posicion_x][posicion_y-1].equals("$")){
+                    
+                            
+                            
+                           
+                            mapa[posicion_x][posicion_y-1]="0";
+                          
+                            rastreo((posicion_x)-tamaño,posicion_y+1);
+                           refresh_map();
+                            posicion_y--;
+                            puntuacion=puntuacion-10;
+                            
+                            refresh_map();
+                        }        
+                        
+                        break;
+                         case "m": menu();
+                         ciclo=false;
+                         break;
+                }
+              
+                 }
                 break;
               
             case "2": 
+                String movi;
+                refresh_map();
+            ciclo=true;
+                while(ciclo){
+                 System.out.println("Escriba su comando porfavor");
+                 movi=leer.next();
+                switch(movi){
+                    
+                    case "a":
+                        if(mapa[posicion_x-1][posicion_y].equals(" "))
+                        {
+                           
+                            mapa[posicion_x-1][posicion_y]="0";
+                          
+                            rastreo((posicion_x-1)+tamaño,posicion_y);
+                           refresh_map();
+                            posicion_x--;
+                            
+                            
+                        }
+                        else if(mapa[posicion_x-1][posicion_y].equals("%")){
+                     mapa[posicion_x-1][posicion_y]="0";
+                            
+                            mapa[posicion_x][posicion_y]="0";
+                            rastreo(posicion_x+1,posicion_y);
+                            refresh_map();
+                            posicion_x--;
+                            puntuacion=puntuacion+10;
+                            tamaño=tamaño+1;
+                            refresh_map();
+                        }             
+                        else if(mapa[posicion_x-1][posicion_y].equals("$")){
+                    
+                            
+                            
+                           
+                            mapa[posicion_x-1][posicion_y]="0";
+                          
+                            rastreo((posicion_x+1)-tamaño,posicion_y);
+                           refresh_map();
+                            posicion_x--;
+                            puntuacion=puntuacion-10;
+                            tamaño--;
+                            refresh_map();
+                        }        
+                        break;
+                    case "d":
+                      if(mapa[posicion_x+1][posicion_y].equals(" "))
+                        {
+                           
+                            mapa[posicion_x+1][posicion_y]="0";
+                          
+                            rastreo((posicion_x+1)-tamaño,posicion_y);
+                          
+                            posicion_x++;
+                             refresh_map();
+                            
+                        }
+                         else if(mapa[posicion_x+1][posicion_y].equals("%")){
+                     mapa[posicion_x+1][posicion_y]="0";
+                            
+                            mapa[posicion_x][posicion_y]="0";
+                            rastreo(posicion_x-1,posicion_y);
+                       
+                            posicion_x++;
+                            puntuacion=puntuacion+10;
+                            tamaño=tamaño+1;
+                            refresh_map();
+                        }             
+                        
+                        
+                      else if(mapa[posicion_x+1][posicion_y].equals("$")){
+                    
+                            
+                            
+                           
+                            mapa[posicion_x+1][posicion_y]="0";
+                          
+                            rastreo((posicion_x+1)-tamaño,posicion_y);
+                           refresh_map();
+                            posicion_x++;
+                            puntuacion=puntuacion-10;
+                            tamaño--;
+                            refresh_map();
+                        }        
+                        break;
+                        
+                        case "s":
+                        if(mapa[posicion_x][posicion_y+1].equals(" "))
+                        {
+                           
+                            mapa[posicion_x][posicion_y+1]="0";
+                          
+                            rastreo((posicion_x),(posicion_y+1)-tamaño);
+                           refresh_map();
+                            posicion_y++;
+                            
+                            
+                        }
+                        else if(mapa[posicion_x][posicion_y+1].equals("%")){
+                     mapa[posicion_x][posicion_y+1]="0";
+                            
+                            mapa[posicion_x][posicion_y]="0";
+                            rastreo(posicion_x,posicion_y-1);
+                          
+                            posicion_y++;
+                            puntuacion=puntuacion+10;
+                            tamaño=tamaño+1;
+                            refresh_map();
+                        }             
+                        else if(mapa[posicion_x][posicion_y+1].equals("$")){
+                    
+                            
+                            
+                           
+                            mapa[posicion_x][posicion_y+1]="0";
+                          
+                            rastreo((posicion_x)-tamaño,posicion_y-1);
+                           refresh_map();
+                            posicion_y++;
+                            puntuacion=puntuacion-10;
+                            tamaño--;
+                            refresh_map();
+                        }        
+                        
+                       
+                        break;
+                         case "w":
+                        if(mapa[posicion_x][posicion_y-1].equals(" "))
+                        {
+                           
+                            mapa[posicion_x][posicion_y-1]="0";
+                          
+                            rastreo((posicion_x),posicion_y-1+tamaño);
+                           refresh_map();
+                            posicion_y--;
+                            
+                            
+                        }
+                        else if(mapa[posicion_x][posicion_y-1].equals("%")){
+                     mapa[posicion_x][posicion_y-1]="0";
+                            
+                            mapa[posicion_x][posicion_y]="0";
+                            rastreo(posicion_x,posicion_y+1);
+                          
+                            posicion_y--;
+                            puntuacion=puntuacion+10;
+                            tamaño=tamaño+1;
+                            refresh_map();
+                        }             
+                        else if(mapa[posicion_x][posicion_y-1].equals("$")){
+                    
+                            
+                            
+                           
+                            mapa[posicion_x][posicion_y-1]="0";
+                          
+                            rastreo((posicion_x)-tamaño,posicion_y+1);
+                           refresh_map();
+                            posicion_y--;
+                            puntuacion=puntuacion-10;
+                            
+                            refresh_map();
+                        }        
+                        
+                        break;
+                         case "m": menu();
+                         ciclo=false;
+                         break;
+                
+              
+               
+              
                  
-                break;
+                }}
+             
             case "3": break;
             case "4":System.exit(0); break;
             default:   
